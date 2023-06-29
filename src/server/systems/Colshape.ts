730306afea13b4a1a72b8102031712b01cc1fa95
CENSOR_1337 <viixxivvimm@gmail.com>
2023-06-30 05:20:51 +0700

    refactor: collshapeSphere now extend from collshape

--- src/server/systems/Colshape.ts
+++ src/server/systems/Colshape.ts
@@ -16,24 +16,21 @@ function GetPlayers() {
 export class Colshape extends WordObject {
 	readonly id = crypto.randomUUID();
 	public playersOnly: boolean;
-	public radius: number;
 	private interval: NodeJS.Timer;
 	private insideEntities: Set<number> = new Set();
 	private destroyed: boolean = false;
-    
-	constructor(pos: Vector3, radius: number) {
+
+	constructor(pos: Vector3) {
 		super(pos);
-		this.radius = radius;
 		this.interval = setInterval(this.onTick.bind(this), 1000);
 	}
 
-	private isPosInside(pos: Vector3) {
-		return this.pos.distance(pos) <= this.radius;
+	protected isPosInside(pos: Vector3) {
+		return false; // implement in child class
 	}
 
-	private isEntityInside(entity: number) {
-		const position = Vector3.fromArray(GetEntityCoords(entity));
-		return this.isPosInside(position);
+	protected isEntityInside(entity: number) {
+		return false; // implement in child class
 	}
 
 	public destroy() {
@@ -79,7 +76,7 @@ export class Colshape extends WordObject {
 		for (const handle of this.insideEntities) {
 			const isExist = DoesEntityExist(handle);
 			const isInside = this.isEntityInside(handle);
-            const isSameDimension = GetEntityRoutingBucket(handle) === this.dimension;
+			const isSameDimension = isExist ? GetEntityRoutingBucket(handle) === this.dimension : false;
 			if (!isExist || !isInside || !isSameDimension) {
 				this.insideEntities.delete(handle);
 				emit(SYSTEM_EVENTS.onLeaveColshape, this.id, handle);
@@ -89,7 +86,7 @@ export class Colshape extends WordObject {
 		for (const handle of entities) {
 			if (this.insideEntities.has(handle)) continue;
 			const isInside = this.isEntityInside(handle);
-            const isSameDimension = GetEntityRoutingBucket(handle) === this.dimension;
+			const isSameDimension = GetEntityRoutingBucket(handle) === this.dimension;
 			if (isInside && isSameDimension) {
 				if (!this.insideEntities.has(handle)) {
 					this.insideEntities.add(handle);
@@ -97,5 +94,22 @@ export class Colshape extends WordObject {
 				}
 			}
 		}
+	}
+}
+
+export class ColshapeSphere extends Colshape {
+	radius: number;
+	constructor(pos: Vector3, radius: number) {
+		super(pos);
+		this.radius = radius;
+	}
+
+	protected isPosInside(pos: Vector3) {
+		return this.pos.distance(pos) <= this.radius;
+	}
+
+	protected isEntityInside(entity: number) {
+		const position = Vector3.fromArray(GetEntityCoords(entity));
+		return this.isPosInside(position);
 	}
 }
